3e2f3b15d6099ded21b595a3cb6f735463a5be0f
Eric Bjurvald <eric@bjurvald.se>
2026-04-29 10:54:27 +0200

    add db integration and seed functionality in astro.config.mjs, package.json, and new db files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  integrations: [db()]
 });
